6fdfcf01b6f45acca2c48e79d6b1276c6fa4a45c
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-08-13 20:51:00 -0400

    wrote Article and User models

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,46 @@
-const { Schema, model, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 const { hash, compare } = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: val => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(val)
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        min: [6, 'Password must contain at least 6 characters']
+    },
+}, {
+    timestamps: true,
+    methods: {
+        async validatePass(formPassword) {
+            const isValid = compare(formPassword, this.password);
+
+            return isValid;
+        }
+    },
+    toJSON: {
+        transform(_, user) {
+            delete user.password;
+            return user;
+        }
+    }
+});
+
+userSchema.pre('save', async function (next) {
+    this.password = await hash(this.password, 10);
+    next();
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
