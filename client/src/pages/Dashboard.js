bda84f35a45630238b5ddfae95946d8612b9cae3
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-08-14 13:24:25 -0400

    dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import axios from 'axios';
 
 
-
+return (
+    <main className="dashboard">
+      <h1 className="text-center">Welcome, {props.state.user.username}!</h1>
 
 
 
