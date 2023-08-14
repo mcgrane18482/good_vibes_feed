58e4d19ecbf33de98f880ee6ca25d337b22f0f2a
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-08-14 13:25:51 -0400

    dahsboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 return (
     <main className="dashboard">
       <h1 className="text-center">Welcome, {props.state.user.username}!</h1>
-
+    </main>
+)
 
 
 export default Dashboard;
