1e7e7eff83ed142bfbc4249e25e07b4dac0d97d6
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-14 22:27:17 -0400

    Refactoring with an iframe, if that doesn't work I'll ditch brainyquote and try something else

--- client/src/components/Quotes.js
+++ client/src/components/Quotes.js
@@ -1,38 +1,6 @@
-import { useEffect, useState } from "react";
+
 
 export default function Quotes() {
-    const [quote, setQuote] = useState({
-        text: "",
-        author: "",
-        authorLink: ""
-    });
-
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://www.brainyquote.com/link/quotebr.js";
-        script.async = true;
-
-        document.body.appendChild(script);
-
-        // Parse quote content at script.src
-        script.onload = () => {
-            const quoteText = document.querySelector("b").nextSibling.innerText.trim();
-            const authorName = document.querySelector("a").innerText;
-            const authorLink = document.querySelector("a").href;
-
-            setQuote({
-                text: quoteText,
-                author: authorName,
-                authorLink
-            });
-        }
-
-        // Should avoid memory leaks by removing the script once it is no longer necessary
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
 
     return (
         <div className="quoteSection">
