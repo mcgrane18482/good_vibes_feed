0dd9e61e4911e589b6b289d5904fedaa8345dc61
Fruit Juice <c.erinmcgrane1@gmail.com>
2023-08-15 19:44:49 -0400

    set up Atlas cloud database

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -9,21 +9,3 @@ export const getOneArticle = async (articleId) => {
     const res = await axios.get(`/api/articles/${articleId}`)
     return res.data;
 }
-
-// export const getArticles = () => {
-//     return fetch('/api/articles', {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//     });
-// };
-
-// export const getOneArticle = (params) => {
-//     return fetch(`/api/articles/${params}`, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//     });
-// };
