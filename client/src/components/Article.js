c1daa82ccf91c485c3d8e7f1d6e9608990c42e24
rpwysocki <134246531+rpwysocki@users.noreply.github.com>
2023-08-15 13:34:58 -0400

    trying to bring in articles, Erin finished first

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,19 +1,12 @@
+import { Article } from 'react-router-dom';
+
 export default function Article() {
     return (
-        <div>
-            <h3>Articles galore!</h3>
-        </div>
-        //  <div className="articles">
-        //             {!article.length && <p>No articles have been added.</p>}
-
-        //             {article.map(article => (
-        //                 <div key={article._id} className="article column">
-        //                     <h3>{article.text}</h3>
-        //                     <div className="column">
-
-        //                     </div>
-        //                 </div>
-        //             ))}
-        //         </div>
+        <>
+            <div>
+                <h3>Articles galore!</h3>
+            </div>
+            
+        </>
     )
 }
