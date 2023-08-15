e27eff9eef471ccae202e15e856388284cb16429
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-15 13:47:25 -0400

    Removed commented out Landing code

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -12,19 +12,6 @@ export default function Landing() {
 
                 <h3>Bringing You Positivity, One Story at a Time</h3>
 
-                {/* <div className="articles">
-                    {!article.length && <p>No articles have been added.</p>}
-
-                    {article.map(article => (
-                        <div key={article._id} className="article column">
-                            <h3>{article.text}</h3>
-                            <div className="column">
-
-                            </div>
-                        </div>
-                    ))}
-                </div> */}
-
                 <Article />
             </main>
         </>
