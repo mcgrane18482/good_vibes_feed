0a85e1a5fb712bc79ef3ed974b99353e272e294f
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-14 22:09:02 -0400

    This should show it, broke down brainyquote's script in useEffect. Not working, but I think we can parse the info at their /link/quotebr.js and set it to a quote state and then use it. Maybe.

--- client/src/components/Quotes.js
+++ client/src/components/Quotes.js
@@ -1,10 +1,29 @@
-
+import { useEffect } from "react";
 
 export default function Quotes() {
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.src = "https://www.brainyquote.com/link/quotebr.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        // Should avoid memory leaks by removing the script once it is no longer necessary
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
     return (
         <div className="quoteSection">
-            <h2 className="quote"><script type="text/javascript" src="https://www.brainyquote.com/link/quotebr.js"></script>
-                <small><i><a href="/quote_of_the_day" target="_blank" rel="nofollow">more Quotes</a></i></small></h2>
+            <h2 className="quote">
+                <small>
+                    <i>
+                        <a href="/quote_of_the_day" target="_blank" rel="nofollow">more Quotes</a>
+                    </i>
+                </small>
+            </h2>
         </div>
     )
 }
