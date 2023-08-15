92d25311187658f70ce04c2462f14c93ef18790b
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-14 21:52:23 -0400

    Just adding their code doesn't work. Didn't think it would, but had to check if I could save some time.

--- client/src/components/Quotes.js
+++ client/src/components/Quotes.js
@@ -3,7 +3,8 @@
 export default function Quotes() {
     return (
         <div className="quoteSection">
-            <h2 className="quote">Quote here</h2>
+            <h2 className="quote"><script type="text/javascript" src="https://www.brainyquote.com/link/quotebr.js"></script>
+                <small><i><a href="/quote_of_the_day" target="_blank" rel="nofollow">more Quotes</a></i></small></h2>
         </div>
     )
 }
