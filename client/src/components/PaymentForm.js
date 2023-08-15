ce5b4d124c36ea3127b00fe216cabf62f3d343ec
Thomas Szentirmay <tlszent@outlook.com>
2023-08-16 01:22:51 +1000

    receiving dummy payments on stripe account successfully

--- client/src/components/PaymentForm.js
+++ client/src/components/PaymentForm.js
@@ -1,4 +1,3 @@
-// StripePayment.js
 import React, { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 import {
@@ -12,6 +11,7 @@ const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
   const [errorMessage, setErrorMessage] = useState(null);
+  const [donationAmount, setDonationAmount] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -20,23 +20,27 @@ const CheckoutForm = () => {
       return;
     }
 
-    // Trigger form validation and wallet collection
     const { error: submitError } = await elements.submit();
     if (submitError) {
-      // Show error to your customer
       setErrorMessage(submitError.message);
       return;
     }
 
+    // Convert donation amount to cents
+    const amountInCents = parseFloat(donationAmount) * 100;
+
     // Create the PaymentIntent and obtain clientSecret from your server endpoint
     const res = await fetch('/create-intent', {
       method: 'POST',
+      body: JSON.stringify({ amount: amountInCents }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
 
     const { client_secret: clientSecret } = await res.json();
 
     const { error } = await stripe.confirmPayment({
-      // `Elements` instance that was used to create the Payment Element
       elements,
       clientSecret,
       confirmParams: {
@@ -45,36 +49,42 @@ const CheckoutForm = () => {
     });
 
     if (error) {
-      // This point will only be reached if there is an immediate error when
-      // confirming the payment. Show error to your customer (for example, payment
-      // details incomplete)
       setErrorMessage(error.message);
     } else {
       // Your customer will be redirected to your `return_url`.
-      // For some payment methods like iDEAL, your customer will be redirected
-      // to an intermediate site first to authorize the payment, then redirected to the `return_url`.
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
+      <label>
+        Donation Amount ($):
+        <input
+          type="number"
+          step="0.01" // Allow decimals for dollars and cents
+          value={donationAmount}
+          onChange={(e) => setDonationAmount(e.target.value)}
+          required
+        />
+      </label>
+
       <PaymentElement />
+
       <button type="submit" disabled={!stripe || !elements}>
         Pay
       </button>
-      {/* Show error message to your customers */}
+
       {errorMessage && <div>{errorMessage}</div>}
     </form>
   );
 };
 
-const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+const stripePromise = loadStripe('pk_test_51NfOh5E0uAKYbFOSiqWMS5UvQr2hskj3tcz2Rnh76OCjP8vXoW0NIFNDxktSP4itivOpsHIRlH6jVGhE4PVe1ORE00PwmjjFkk');
 
 const options = {
   mode: 'payment',
-  amount: 1099,
   currency: 'usd',
-  // Fully customizable with appearance API.
+  amount: 1099,
   appearance: {
     /*...*/
   },
