ac3cbf509bfbd85997adbc11d07b3780c2ab1ae0
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-15 10:33:49 -0400

    Git no like me.

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -11,7 +11,23 @@ export default function Landing() {
 
                 <h3>Bringing You Positivity, One Story at a Time</h3>
 
+
+                <h3>Bringing You Positivity, One Story at a Time</h3>
+
+                {/* <div className="articles">
+                    {!article.length && <p>No articles have been added.</p>}
+
+                    {article.map(article => (
+                        <div key={article._id} className="article column">
+                            <h3>{article.text}</h3>
+                            <div className="column">
+
+                            </div>
+                        </div>
+                    ))}
+                </div> */}
                 <Article />
+
             </main>
         </>
     )
