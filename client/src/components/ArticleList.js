91367f9fbe0a078abbc23142539ecebb815210de
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-15 15:18:21 -0400

    Found one more issue, but I think I got all the changes now

--- client/src/components/ArticleList.js
+++ client/src/components/ArticleList.js
@@ -26,19 +26,21 @@ export default function ArticleList() {
     return (
 
 
-        <div>
+        <div className='articles'>
             <h3>Articles</h3>
-            {articles.map((article) => {
-                return (
-                    <NavLink to={`/article/${article._id}`} key={article._id}>
-                        <h3>{article.title}</h3>
-                        <img>{article.image}</img>
-                        <h5>{article.description}</h5>
-                        <p>{article.content}</p>
-                        <p>{article.comments}</p>
-                    </NavLink>
-                )
-            })}
+            <div className='card'>
+                {articles.map((article) => {
+                    return (
+                        <NavLink to={`/article/${article._id}`} key={article._id}>
+                            <h3>{article.title}</h3>
+                            <img>{article.image}</img>
+                            <h5>{article.description}</h5>
+                            <p>{article.content}</p>
+                            <p>{article.comments}</p>
+                        </NavLink>
+                    )
+                })}
+            </div>
         </div>
     );
 };
