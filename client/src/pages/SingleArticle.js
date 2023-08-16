36370856da9c7346dab2a0212c2ed1c958069661
Thomas Szentirmay <tlszent@outlook.com>
2023-08-17 04:42:53 +1000

    added styling to single article pages to match landing

--- client/src/pages/SingleArticle.js
+++ client/src/pages/SingleArticle.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getOneArticle } from '../utils/api';
 import { useParams } from "react-router-dom";
 
@@ -9,14 +9,12 @@ export default function SingleArticle() {
     useEffect(() => {
         const getArticle = async () => {
             try {
-                console.log(params.id)
                 const articleData = await getOneArticle(params.id);
                 if (!articleData) {
                     throw new Error('No article was found with that id')
                 }
 
                 setArticle(articleData);
-                console.log(article.urlToImage);
             } catch (err) {
                 console.log(err);
             }
@@ -25,17 +23,24 @@ export default function SingleArticle() {
     }, [params.id]);
 
     return (
-        <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center py-10">
-            <div className="max-w-4xl bg-white rounded-lg shadow-lg p-8 w-full">
-                <h3 className="text-xl font-semibold mb-2">{article.title}</h3>
-                <img src={article.urlToImage} alt='' className="w-full rounded-lg mb-4" />
-                <h5 className="text-lg mb-2">{article.description}</h5>
-                <p className="mb-4">{article.content}</p>
-                <a target="_blank" rel="noopener noreferrer" href={article.url} className="text-blue-500 hover:underline mb-4">
-                    {article.url}
-                </a>
-                <p className="mb-4">Comments: {article.comments}</p>
-            </div>
+        <div className="bg-gradient-to-b from-gray-800 to-gray-900 text-white min-h-screen">
+            <main className="max-w-5xl mx-auto px-4 py-12">
+                <div className="bg-gray-600 rounded-lg shadow-lg p-8">
+                    <h3 className="text-xl md:text-2xl font-semibold mb-2">{article.title}</h3>
+                    <img src={article.urlToImage} alt='' className="w-full rounded-lg mb-4" />
+                    <h5 className="text-lg md:text-xl mb-2">{article.description}</h5>
+                    <p className="mb-4">{article.content}</p>
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={article.url}
+                        className="text-blue-500 hover:underline mb-4"
+                    >
+                        {article.url}
+                    </a>
+                    <p className="mb-4">Comments: {article.comments}</p>
+                </div>
+            </main>
         </div>
     )
 }
