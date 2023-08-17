d42cce05254944fa852bdcf7353dfcfda2ff91d2
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-17 10:12:03 -0400

    Apostrophe

--- client/src/components/ArticleList.js
+++ client/src/components/ArticleList.js
@@ -23,7 +23,7 @@ export default function ArticleList() {
 
     return (
         <div className="articles p-6">
-            <h3 className="text-2xl font-semibold mb-4">Todays Postive Articles</h3>
+            <h3 className="text-2xl font-semibold mb-4">Today's Postive Articles</h3>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {articles.map((article) => (
                     <NavLink
